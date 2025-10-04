688cb0c5a3d471efd4ca1a0080d82229b408e36b
devil <devil@gmail.com>
2025-10-04 13:57:47 +0530

    fix

--- services/api-url.js
+++ services/api-url.js
@@ -12,8 +12,8 @@ if (process.env.REACT_APP_SERVER?.trim() == "production") {
   ROOT_URL = "http://localhost:5656/api/v1";
   SOCKET_URL = "http://localhost:5656";
 }
-  // ROOT_URL = "http://staging.alphonic.net.in:5656/api/v1";
-  // SOCKET_URL = "http://staging.alphonic.net.in:5656";
+  ROOT_URL = "http://13.236.202.246:3000/api/v1";
+  SOCKET_URL = "http://13.236.202.246:3000";
 
 export { IMAGE_URL, SOCKET_URL };
 export default ROOT_URL;

918f8b681bfbe47585669df82ea82323656cdcf8
Ajay Sharma <ajaysharma756891@gmail.com>
2025-10-09 15:08:48 +0530

    Change Api URL

--- services/api-url.js
+++ services/api-url.js
@@ -3,8 +3,8 @@ let IMAGE_URL = "";
 let SOCKET_URL = "";
 
 if (process.env.REACT_APP_SERVER?.trim() == "production") {
-    ROOT_URL = 'http://13.236.202.246:3000/api/v1'
-    SOCKET_URL = 'http://13.236.202.246:3000'
+  ROOT_URL = "http://13.236.202.246:3000/api/v1";
+  SOCKET_URL = "http://13.236.202.246:3000";
 } else if (process.env.REACT_APP_SERVER?.trim() == "staging") {
   ROOT_URL = "http://staging.alphonic.net.in:5656/api/v1";
   SOCKET_URL = "http://staging.alphonic.net.in:5656";
@@ -12,8 +12,10 @@ if (process.env.REACT_APP_SERVER?.trim() == "production") {
   ROOT_URL = "http://localhost:5656/api/v1";
   SOCKET_URL = "http://localhost:5656";
 }
-  ROOT_URL = "http://13.236.202.246:3000/api/v1";
-  SOCKET_URL = "http://13.236.202.246:3000";
+// ROOT_URL = "http://13.236.202.246:3000/api/v1";
+// SOCKET_URL = "http://13.236.202.246:3000";
+ROOT_URL = "https://api.furrbaby.in/api/v1";
+SOCKET_URL = "https://api.furrbaby.in";
 
 export { IMAGE_URL, SOCKET_URL };
 export default ROOT_URL;
